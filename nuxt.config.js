38d26041276634edc1df47af9d670272fdd02d9e
Matt Larson <matt.a.larson@gmail.com>
2018-06-16 08:46:01 -0500

    added page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'nuxt-portfolio',
+    title: 'Matt Larson Portfolio',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
